06dbbfc49fe0c1cd633fe10ec9642ff3808f3c1d
Oleh Kolinko <oleh.kolinko@gmail.com>
2017-02-10 17:32:08 -0500

    Added major gameplay logic

--- js/app.js
+++ js/app.js
@@ -1,11 +1,31 @@
 
 var game;
 $(document).ready(function(){
-  game = new ConnectFour('Oleh','Stas');
-  renderBoard();
+  $('#start-btn').click(function(e){
+    e.preventDefault();
+    var player1 = $('#input-pl-1').val();//Get pl1 name
+    var player2 = $('#input-pl-2').val();//Get pl2 name
+
+    game = new ConnectFour(player1 , player2);//Create new game
+    updateCurrentPlayer();
+
+    //Show playing board:
+    $('#player-turn').fadeIn(1000);
+    $('.game-board').fadeIn(1000);
+    $('#player-form').slideUp(500);
+  });
+
+  //play
+  $('.col').click(function(){
+    var colN = $(this).attr('id');
+    colN = parseInt(colN[colN.length-1]);//get columnNumber
+    game.playChecker(colN);
+    renderBoard();
+    updateCurrentPlayer();
+  });
 });
 
-function renderBoard(){
+function renderBoard(){ //Show all checkers on DOM
   game.board.forEach(function(row,i){
     row.forEach(function(slot,j){
 
@@ -17,9 +37,17 @@ function renderBoard(){
       }else{
         //empty
       }
-
-
-
     });
   });
+}
+
+function updateCurrentPlayer(){ //Update player turn on DOM
+  $('#player').html(game.currentPlayer);
+  if(game.currentPlayer === game.player1){
+    $('#player').parent().removeClass('player-2');
+    $('#player').parent().addClass('player-1');
+  }else{
+    $('#player').parent().removeClass('player-1');
+    $('#player').parent().addClass('player-2');
+  }
 }
--- js/connect-four.js
+++ js/connect-four.js
@@ -1,20 +1,47 @@
 function ConnectFour(player1Name, player2Name){
   this.board = [
     //0    1    2    3    4    5    6
-    [null,null,null,null,2,null,null],//0
+    [null,null,null,null,null,null,null],//0
     [null,null,null,null,null,null,null],//1
-    [null,1,null,1,null,null,null],//2
+    [null,null,null,null,null,null,null],//2
     [null,null,null,null,null,null,null],//3
-    [null,null,2,null,null,null,null],//4
-    [1,null,null,null,null,null,null],//5
+    [null,null,null,null,null,null,null],//4
+    [null,null,null,null,null,null,null],//5
   ];
   this.player1 = player1Name;
   this.player2 = player2Name;
   this.winner = null;
 
+  //Random player for first turn:
   if(Math.random()<0.5){
     this.currentPlayer = this.player1;
   }else{
     this.currentPlayer = this.player2;
   }
 }
+
+ConnectFour.prototype.playChecker = function(columnNumber){
+  for(var i = this.board.length - 1; i >= 0 ; i--){
+    if(this.board[i][columnNumber] === null){
+      if(this.currentPlayer === this.player1){
+        this.board[i][columnNumber] = 1;//Player's 1 turn
+        this.toogleCurrentPlayer();
+        return true;
+      }else{
+        this.board[i][columnNumber] = 2;//Player's 2 turn
+        this.toogleCurrentPlayer();
+        return true;
+      }
+    }
+  }
+  console.log('No more space in column');
+  return false;
+};
+
+ConnectFour.prototype.toogleCurrentPlayer = function(){
+  if (this.currentPlayer === this.player1) {
+    this.currentPlayer = this.player2;
+  }else{
+    this.currentPlayer = this.player1;
+  }
+};
